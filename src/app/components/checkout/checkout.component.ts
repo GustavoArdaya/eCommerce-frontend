3582763da325e96848201ce94ca849dd9aac2cf3
GustavoArdaya <gustavo.ardaya@gmail.com>
2023-11-16 10:44:05 +0100

    add handleMonthsAndYears to checkout component

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { CommerceFormService } from 'src/app/services/commerce-form.service';
 
 @Component({
   selector: 'app-checkout',
@@ -13,7 +14,12 @@ export class CheckoutComponent implements OnInit {
   totalPrice: number = 0;
   totalQuantity: number = 0;
 
-  constructor(private formBuilder: FormBuilder) {
+  creditCardYears: number[] = [];
+  creditCardMonths: number[] = [];
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private commerceFormService: CommerceFormService) {
     
   }
 
@@ -46,7 +52,22 @@ export class CheckoutComponent implements OnInit {
         expirationMonth: [''],
         expirationYear: ['']
       })
-    })
+    });
+
+    // populate credit card months and years dropdowns
+
+    const startMonth: number = new Date().getMonth() + 1;
+    console.log("startMonth: " + startMonth);
+
+    this.commerceFormService.getCreditCardMonths(startMonth).subscribe(data => {
+      console.log("Retrieved credit card months: " + JSON.stringify(data));
+      this.creditCardMonths = data;
+    });
+
+    this.commerceFormService.getCreditCardYears().subscribe(data => {
+      console.log("Retrieved credit card years: " + JSON.stringify(data));
+      this.creditCardYears = data;
+    });
   }
 
   onSubmit() {
@@ -63,5 +84,20 @@ export class CheckoutComponent implements OnInit {
     } else {
       this.checkoutFormGroup.controls['billingAddress'].reset();
     }
+  }
+
+  handleMonthsAndYears() {
+
+    const creditCardFormGroup = this.checkoutFormGroup.get('creditCard');
+
+    const currentYear: number = new Date().getFullYear();
+    const selectedYear: number = Number(creditCardFormGroup?.value.expirationYear);
+
+    let startMonth: number = (currentYear == selectedYear)? new Date().getMonth() + 1 : 1;
+
+    this.commerceFormService.getCreditCardMonths(startMonth).subscribe(data => {
+      console.log("Retrieved credit card months: " + JSON.stringify(data));
+      this.creditCardMonths = data;
+    })
   }
 }
