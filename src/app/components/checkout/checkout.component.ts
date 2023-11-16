c73a18551bc5ee5ba63b21fc303e026efa4c092e
GustavoArdaya <gustavo.ardaya@gmail.com>
2023-11-16 10:09:15 +0100

    add checkout component reactive form

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -10,6 +10,9 @@ export class CheckoutComponent implements OnInit {
   
   checkoutFormGroup!: FormGroup;
 
+  totalPrice: number = 0;
+  totalQuantity: number = 0;
+
   constructor(private formBuilder: FormBuilder) {
     
   }
@@ -20,7 +23,45 @@ export class CheckoutComponent implements OnInit {
         firstName: [''],
         lastName: [''],
         email: ['']
+      }),
+      shippingAddress: this.formBuilder.group({
+        street: [''],
+        city: [''],
+        state: [''],
+        country: [''],
+        zipCode: ['']
+      }),
+      billingAddress: this.formBuilder.group({
+        street: [''],
+        city: [''],
+        state: [''],
+        country: [''],
+        zipCode: ['']
+      }),
+      creditCard: this.formBuilder.group({
+        cardType: [''],
+        nameOnCard: [''],
+        cardNumber: [''],
+        securityCode: [''],
+        expirationMonth: [''],
+        expirationYear: ['']
       })
     })
+  }
+
+  onSubmit() {
+    console.log('Handling form botton');
+    console.log('customer: ' + this.checkoutFormGroup.get('customer')!.value);
+    console.log('billing address:' + this.checkoutFormGroup.get('billingAddress')!.value);
+    console.log('shipping address' + this.checkoutFormGroup.get('shippingAddress')!.value);
+  }
+
+  copyShippingAddressToBillingAddress($event: any) {
+    if ($event.target.checked) {
+      this.checkoutFormGroup.controls['billingAddress']
+      .setValue(this.checkoutFormGroup.controls['shippingAddress'].value);
+    } else {
+      this.checkoutFormGroup.controls['billingAddress'].reset();
+    }
   }
 }
