db1e4bbe88022d132b81330aecf2a0576281b185
GustavoArdaya <gustavo.ardaya@gmail.com>
2023-11-22 11:48:51 +0100

    fixed previous error in regex expression that was causing whole form to fail. escaped minus sign

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -37,7 +37,7 @@ export class CheckoutComponent implements OnInit {
         firstName: new FormControl('', [Validators.required, Validators.minLength(2), EcommerceValidators.notOnlyWhitespace]),
         lastName: new FormControl('', [Validators.required, Validators.minLength(2), EcommerceValidators.notOnlyWhitespace]),
         email: new FormControl('', 
-          [Validators.required, Validators.pattern('^[a-z0-9.-%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]
+          [Validators.required, Validators.pattern('^[a-z0-9.%+\\-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]
         )
       }),
       shippingAddress: this.formBuilder.group({
