0955044d859ca22825fec77a5f5177ecbefbc695
GustavoArdaya <gustavo.ardaya@gmail.com>
2023-11-28 10:21:34 +0100

    add okta dependencies, configuration and css to angular.json

---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,9 @@
+export default {
+
+    oidc: {
+        clientId: '0oadimw052tgNNaWQ5d7',
+        issuer: 'https://dev-72496107.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+}
