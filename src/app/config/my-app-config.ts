9f899dcb5a2ddbfb30bd3c5945ed659587882d04
GustavoArdaya <gustavo.ardaya@gmail.com>
2023-12-07 11:43:34 +0100

    change redirectUri in my-app-config

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -3,7 +3,7 @@ export default {
     oidc: {
         clientId: '0oadimw052tgNNaWQ5d7',
         issuer: 'https://dev-72496107.okta.com/oauth2/default',
-        redirectUri: 'http://localhost:4200/login/callback',
+        redirectUri: 'https://localhost:4200/login/callback',
         scopes: ['openid', 'profile', 'email']
     }
 }
