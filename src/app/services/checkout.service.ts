fda7c303363370d414a49abfeccd5732230bb712
GustavoArdaya <gustavo.ardaya@gmail.com>
2023-12-10 09:10:05 +0100

    add creayePaymentIntent method on checkout service

--- src/app/services/checkout.service.ts
+++ src/app/services/checkout.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Purchase } from '../common/purchase';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { PaymentInfo } from '../common/payment-info';
 
 @Injectable({
   providedIn: 'root'
@@ -16,5 +17,9 @@ export class CheckoutService {
 
   placeOrder(purchase: Purchase): Observable<any> {
     return this.http.post<Purchase>(this.purchaseUrl, purchase);
+  }
+
+  createPaymentIntent(paymentInfo: PaymentInfo): Observable<any> {
+    return this.http.post<PaymentInfo>(this.paymentIntentUrl, paymentInfo);
   }
 }
