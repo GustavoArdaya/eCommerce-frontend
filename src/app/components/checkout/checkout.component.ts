63630b7778650a2fd31283f764650045ae42a2ad
GustavoArdaya <gustavo.ardaya@gmail.com>
2023-12-17 11:04:58 +0100

    fix bug that sent incorrect value to backend on checkout component's on submit

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -244,7 +244,7 @@ export class CheckoutComponent implements OnInit {
     purchase.orderItems = orderItems;
 
     // compute payment info
-    this.paymentInfo.amount = this.totalPrice * 100;
+    this.paymentInfo.amount = Math.round(this.totalPrice * 100);
     this.paymentInfo.currency = "USD";
 
     // if form is valid, create payment intent, confirm card payment and place order
